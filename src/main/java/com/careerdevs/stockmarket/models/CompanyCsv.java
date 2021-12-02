407928aaedbc287752cbd809a30a966ed7bd34cd
unknown <22105805+enjee1@users.noreply.github.com>
2021-12-02 18:42:03 -0500

    Added CompanyCsv model, StockCsvParser and data_set_1 and _2 csv files

---
+++ src/main/java/com/careerdevs/stockmarket/models/CompanyCsv.java
@@ -0,0 +1,58 @@
+package com.careerdevs.stockmarket.models;
+
+public class CompanyCsv {
+    private String symbol;
+    private String name;
+    private String exchange;
+    private String assetType;
+    private String ipoDate;
+    private String delistingDate;
+    private String status;
+
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getExchange() {
+        return exchange;
+    }
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+    public String getAssetType() {
+        return assetType;
+    }
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+    public String getIpoDate() {
+        return ipoDate;
+    }
+    public void setIpoDate(String ipoDate) {
+        this.ipoDate = ipoDate;
+    }
+    public String getDelistingDate() {
+        return delistingDate;
+    }
+    public void setDelistingDate(String delistingDate) {
+        this.delistingDate = delistingDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    @Override
+    public String toString() {
+        return "{" + name + "::" + symbol + "::" + ipoDate + "::" + status + "}";
+    }
+}
---
+++ src/main/java/com/careerdevs/stockmarket/parsers/StockCsvParser.java
@@ -0,0 +1,4 @@
+package com.careerdevs.stockmarket.parsers;
+
+public class StockCsvParser {
+}
